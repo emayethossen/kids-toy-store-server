41b74ae8f0f244da8243c879ed1ada1547445eb8
emayethossen <samir2021hossen@gmail.com>
2023-05-19 08:31:14 +0600

    delete oparetion added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,13 @@ async function run() {
             console.log(newToy);
             const result = await toyCollection.insertOne(newToy);
             res.send(result)
+        })
+
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
